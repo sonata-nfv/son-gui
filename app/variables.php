eef656c16933f4f677e3ea3b496b483dbf6a381f
Manos Zacharioudakis <zacharioudakis@synelixis.com>
2017-08-01 09:14:55 +0100

    back to old api urls

--- app/variables.php
+++ app/variables.php
@@ -2,16 +2,16 @@
 
 $vars = array();
 $debug=false;
-/*
+
 $vars['MON_URL']=($debug==true?'http://192.168.1.127:8000':getenv('MON_URL'));
 $vars['GK_URL']=($debug==true?'https://sp.int3.sonata-nfv.eu/api/v2':getenv('GK_URL'));
 $vars['LOGS_URL']=($debug==true?'http://logs.sonata-nfv.eu:12900':getenv('LOGS_URL'));
 $vars['VIMS_URL']=($debug==true?'https://sp.int3.sonata-nfv.eu/api/v2':getenv('GK_URL'));
-*/
-$vars['MON_URL']=($debug==true?'http://192.168.1.127:8000':url());
+
+/*$vars['MON_URL']=($debug==true?'http://192.168.1.127:8000':url());
 $vars['GK_URL']=($debug==true?'https://sp.int3.sonata-nfv.eu/api/v2':url());
 $vars['LOGS_URL']=($debug==true?'http://logs.sonata-nfv.eu:12900':url());
-$vars['VIMS_URL']=($debug==true?'https://sp.int3.sonata-nfv.eu/api/v2':url());
+$vars['VIMS_URL']=($debug==true?'https://sp.int3.sonata-nfv.eu/api/v2':url());*/
 
 echo json_encode($vars);
 
